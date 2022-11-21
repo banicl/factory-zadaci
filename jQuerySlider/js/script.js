aad9bdefb7386f89f7e43aa45bd1a7e68bba4a60
Leonarda Banić <banic.leonarda2@gmail.com>
2022-11-21 02:21:16 +0100

    Slider Update

---
+++ jQuerySlider/js/script.js
@@ -0,0 +1,21 @@
+var container = $(".container");
+var ulFind = container.find('ul');
+var liWidth = ulFind.find('li:first').width();
+var PageItms = 5;
+var itmNo = ulFind.find('li').length;
+
+ulFind.width(itmNo * liWidth);
+container.width(PageItms * liWidth);
+
+$(document).ready(function () {
+    $('.arrow-right').click(function () {
+        $('ul').animate({ "left": "+=100px" }, 300);
+        console.log("Ok.");
+    });
+});
+$(document).ready(function () {
+    $('.arrow-left').click(function () {
+        $('ul').animate({ "left": "-=100px" }, 300);
+        console.log("Ok.");
+    });
+});
